cc369cd2f828654074c6462087758af5b8bf42cb
cody brunty <codybrunty@gmail.com>
2020-05-13 18:43:54 -0700

    Added TMPro and Device LogViewer
    Added the text mesh pro package and a logviewer for debuging on device.

---
+++ JumpyBoi/Assets/EasyLogView/Scripts/EasyLogView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace nmxi.easylogview{
+    public class EasyLogView : MonoBehaviour {
+        [SerializeField]
+        private TextMeshProUGUI m_textUI;
+
+        private void Awake(){
+            m_textUI = GetComponent<TextMeshProUGUI>();
+            Application.logMessageReceived += OnLogMessage;
+            
+            m_textUI.text = String.Empty;
+        }
+
+        private void OnDestroy()
+        {
+            Application.logMessageReceived -= OnLogMessage;
+        }
+
+        private void OnLogMessage( string i_logText, string i_stackTrace, LogType type )
+        {
+            var tmp = m_textUI.text;
+            m_textUI.text = "[" + DateTime.Now.ToLongTimeString() + "] ";
+            switch (type){
+                case LogType.Warning:
+                    m_textUI.text += "<color=#ffff00>" + i_logText + "</color>" + Environment.NewLine;
+                    break;
+                case LogType.Error:
+                    m_textUI.text += "<color=#ff0000>" + i_logText + "</color>" + Environment.NewLine;
+                    break;
+                default:
+                    m_textUI.text += i_logText + Environment.NewLine;
+                    break;
+            }
+            m_textUI.text += tmp;
+        }
+
+        public void ResetLog(){
+            m_textUI.text = "[" + DateTime.Now.ToLongTimeString() + "] Clear log";
+        }
+    }
+}
